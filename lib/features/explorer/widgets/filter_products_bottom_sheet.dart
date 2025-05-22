e1c695c8ca87d378ebad9f98679460734c77e190
amina-wq <kannami2004@gmail.com>
2025-05-22 21:28:41 +0300

    updated dependencies

--- lib/features/explorer/widgets/filter_products_bottom_sheet.dart
+++ lib/features/explorer/widgets/filter_products_bottom_sheet.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:buy_buy/features/explorer/bloc/bloc.dart';
 import 'package:buy_buy/models/models.dart';
-import 'package:buy_buy/repositories/product/product_repository_interface.dart';
 import 'package:buy_buy/ui/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
