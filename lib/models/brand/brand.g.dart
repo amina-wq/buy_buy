96347ecb1aca122666fe4e57acad1f0fcc5d5932
amina-wq <kannami2004@gmail.com>
2025-05-22 21:29:58 +0300

    brand.g file

---
+++ lib/models/brand/brand.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'brand.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Brand _$BrandFromJson(Map<String, dynamic> json) =>
+    Brand(id: json['documentID'] as String, name: json['name'] as String);
+
+Map<String, dynamic> _$BrandToJson(Brand instance) => <String, dynamic>{
+  'documentID': instance.id,
+  'name': instance.name,
+};
